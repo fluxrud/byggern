5e12f4ce6e7621ea56a96b4b009eca4add4c0ed9
Anders Breistøl <andbreis@ntnu.no>
2022-08-31 10:10:22 +0200

    created project

---
+++ byggern_proj/byggern_proj/main.c
@@ -0,0 +1,17 @@
+/*
+ * byggern_proj.c
+ *
+ * Created: 31.08.2022 10:09:11
+ * Author : andbreis
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
