dbacea179888968a035128ca77e7cec421abfb49
Andreas Flugsrud Dahl <andrea7@ntnu.no>
2022-10-12 11:58:17 +0200

    created node 2 project

---
+++ node_2/node_2/main.c
@@ -0,0 +1,21 @@
+/*
+ * node_2.c
+ *
+ * Created: 12.10.2022 11:55:49
+ * Author : andrea7
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
